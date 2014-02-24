f1c643360a08be77fb177610654a8327ed64d55f
christianHuygens <christian.huygens@gmail.com>
2014-02-24 18:36:27 -0200

    Update and rename Login.java to Usuario.java

---
+++ Usuario.java
@@ -0,0 +1,26 @@
+package ifrn.poo.projetoLembretes;
+
+public class Usuario {
+
+	private String login;
+	private String senha;
+	
+	public Usuario(String entradaL, String entradaS){
+		this.login = entradaL;
+		this.senha = entradaS;
+	}
+	
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
